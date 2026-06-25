e8d95f07967eada9b00d6c06cec5f243c8e4f297
s.ding@latrobe.edu.au <s.ding@latrobe.edu.au>
2026-06-25 22:02:06 +1000

    Clear repository contents

--- README.md
+++
@@ -1,2 +0,0 @@
-# CSE3CWA-5006-Week-06
-Cloud based Web Application Week 6
--- modern_react_dashboard/src/components/MetricCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function MetricCard({ label, value, helper }) {
-  return (
-    <article className="metric-card">
-      <span>{label}</span>
-      <strong>{value}</strong>
-      <p>{helper}</p>
-    </article>
-  );
-}
